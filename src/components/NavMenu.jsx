7627041ae2e40b46a0f22500400a00d7a232dcdf
Ivan <ivanvoloshiin@mail.ru>
2022-12-08 21:37:27 +0300

    s

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand, NavLink } from 'reactstrap';
+import { Navbar, Nav, Offcanvas} from 'react-bootstrap';
+import { NavLink} from 'reactstrap';
 import { Link } from 'react-router-dom';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
+// import Offcanvas from 'react-bootstrap/Offcanvas';
 import './NavMenu.css';
-import favicon from '../Images/favicon.svg';
 
 export class NavMenu extends Component {
     static displayName = NavMenu.name;
@@ -34,23 +37,46 @@ export class NavMenu extends Component {
         this.setState({
             collapsed: !this.state.collapsed
         });
-}
 
+    }
+    
+    home () {
+        window.location.href("/");
+    };
+    direction () {
+        window.location.href("/Direction");
+    };
+    contact () {
+        window.location.href("/Contact");
+    };
 
     render() {
             return (
-                <header id="header" className="navbar-outer navbar-expand-lg  fixed-top">
-                    <Navbar className="container-fluid navbar-inner text-bold" container light>
-                        <div className="flex-grow-1">
-                            <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-                                <NavLink tag={Link} className="text-main" to="/">Бюджет</NavLink>
-                                <NavLink tag={Link} className="text-main" to="/Direction">Направления</NavLink>
-                            </ul>
+                <header className='navbar-outer navbar-expand-lg fixed-top'>
+                <Navbar expand="sm" className='container-fluid navbar-inner'>
+                    <div className='container'>
+                    <Navbar.Brand className='navbar-brand' href="/BudgetMos/">Бюджет <br/> для <br/> граждан</Navbar.Brand>
+                    <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-sm`} />
+                    <Navbar.Offcanvas id={`offcanvasNavbar-expand-sm`} aria-labelledby={`offcanvasNavbarLabel-expand-sm`} placement="end">
+                        <div className='offcanvas-item'>
+                        <Offcanvas.Header closeButton>
+                            <Offcanvas.Title id={`offcanvasNavbarLabel-expand-sm`} className="text-title">
+                            <h1 className='text-main'>Бюджет для граждан</h1>
+                            </Offcanvas.Title>
+                        </Offcanvas.Header>
+                        <Offcanvas.Body>
+                            <Nav className="flex-grow-1 pe-3">
+                                <NavLink tag={ Link } className = "text-main" to="/" onClick={this.home} >Бюджет</NavLink>
+                                <NavLink tag={ Link } className = "text-main" to="/Direction" onClick={this.direction}>Направления</NavLink>
+                            </Nav>
+                            <Nav className="flex-grow-2 pe-3">
+                                <NavLink tag={ Link } className = "text-main nav-link" to ="/Contact" onClick={this.contact}>Контакты</NavLink>
+                            </Nav>
+                        </Offcanvas.Body>
                         </div>
-                        <div className="flex-grow-2 ">
-                                <NavLink tag={Link} className="text-main" to="/Contact">Контакты</NavLink>
-                        </div>  
-                    </Navbar>
+                    </Navbar.Offcanvas> 
+                    </div>
+                </Navbar>
                 </header>
             );
         }
